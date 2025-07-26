3f5780f37d3503792e86099c103debbb24d22538
int-code <pubalibasak16@gmail.com>
2025-07-26 18:26:45 +0000

    feat: backend - add trigger workflow

--- backend/utils/pr_db_actions.py
+++ backend/utils/pr_db_actions.py
@@ -52,17 +52,75 @@ async def check_pr_mergeable(data):
             raise e
 
 
+async def trigger_workflow(repo_name, owner, action_workflow_filename, base_branch, head_branch, GITHUB_TOKEN):
+
+    # Trigger GitHub Action workflow_dispatch
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/actions/workflows/{action_workflow_filename}/dispatches"
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json"
+    }
+    json_data = {
+        "ref":base_branch,  # run action on base branch (e.g., main)
+        "inputs": {
+            "head_ref": head_branch,
+            "base_ref": base_branch
+        }
+    }
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(url, headers=headers, json=json_data)
+        if resp.status_code != 204:
+            raise Exception(f"Failed to trigger workflow: {resp.text}")
+
+async def verify_repo_access(token, owner, repo_name):
+    url = f"https://api.github.com/repos/{owner}/{repo_name}"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github+json"
+    }
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, headers=headers)
+        if resp.status_code != 200:
+            raise Exception(f"No access to repository: {resp.text}")
+
+async def check_workflow_exists(token, owner, repo_name, workflow_filename):
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/actions/workflows"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github+json"
+    }
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, headers=headers)
+        workflows = resp.json()['workflows']
+        print(workflows)
+        return any(w["name"] == workflow_filename or 
+                  w["path"].endswith(workflow_filename) 
+                  for w in workflows)
+
+
+async def get_installation_details(jwt_token, installation_id):
+    url = f"https://api.github.com/app/installations/{installation_id}"
+    headers = {
+        "Authorization": f"Bearer {jwt_token}",
+        "Accept": "application/vnd.github+json"
+    }
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"Failed to get installation: {response.text}")
+        return response.json()
+
+
 @celery_app.task
 def handle_new_pr(data):
     try:
         mergeable = asyncio.run(check_pr_mergeable(data))
         if not mergeable:
-            resolve_merge_conflicts.delay(data)
+            installation_token = asyncio.run(get_or_refresh_installation_token(data["installation"]["id"]))
+            asyncio.run(trigger_workflow(data['repository']["name"], data['repository']["owner"]['name'], "merge-conflict.yaml", data["pull_request"]["base"]["ref"], data["pull_request"]["head"]["ref"], installation_token))
     except Exception as e:
         print(f"Error handling new PR: {e}")
         raise Exception("Error processing PR data")
     
-
-@celery_app.task
-def resolve_merge_conflicts(data):
-    pass
